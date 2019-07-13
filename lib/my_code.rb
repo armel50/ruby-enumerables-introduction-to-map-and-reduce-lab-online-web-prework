f59f7c8570eb179004bfb47539a58f29c06b4aa4
Cloe Kouadjo <armelfli50@gmail.com>
2019-07-13 15:55:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,7 +52,13 @@ def reduce_to_total(source_array, starting_point=0)
 end
 
 def reduce_to_all_true(source_array) 
-  
+   counter = 0
+  while source_array[counter] do
+    if source_array[counter] == false
+      return false
+      
+    counter += 1
+  end
 end
 
 def reduce_to_any_true(source_array)
